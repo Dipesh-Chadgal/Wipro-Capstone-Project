5a4469230c54333cff5dd11232d0456c71d46416
Dipesh-Chadgal <dipeshchadgal10@gmail.com>
2025-03-16 21:05:45 +0530

    Added comment for the method to be implemented

--- src/main/java/com/controller/BlogController.java
+++ src/main/java/com/controller/BlogController.java
@@ -21,6 +21,19 @@ public class BlogController {
 	/*
 	 * =============================================================================
 	 * GET AND POST MAPPING TO FETCH ALL BLOGS AND TO ADD A NEW BLOG
+	 * 
+	 * 
+	 * GET
+	 * 
+	 * If Blog are present then Response code: 200 (OK) If No Blog are present then
+	 * Response code: 204 ( No Content)
+	 * 
+	 * 
+	 * POST
+	 * 
+	 * Successful Blog creation Response code: 201 (Created) If title or content is
+	 * blank then Response code: 400 (Bad request)
+	 * 
 	 */
 
 	@GetMapping("/blogs")
@@ -37,7 +50,26 @@ public class BlogController {
 
 	/*
 	 * =============================================================================
+	 * GET MAPPING FOR FETCHING AN EXISTING BLOG
+	 * 
+	 * If blog id is valid then Response code: 200 (OK) 
+	 * If blog id is invalid then Response code: 404 (Not Found)
+	 * 
+	 */
+
+	@GetMapping("/blogs/{id}")
+	public ResponseEntity<BlogDto> getBlogById() {
+
+		return null;
+	}
+
+	/*
+	 * =============================================================================
 	 * PUT MAPPING FOR UPDATING AN EXISTING BLOG
+	 * 
+	 * If blog id is valid and update is successful then Response code: 200 (OK) If
+	 * blog id is invalid then Response code: 404 (Not Found) 
+	 * If request body is invalid then Response code: 400 (Bad Request)
 	 */
 
 	@PutMapping("/blogs/{id}")
@@ -49,7 +81,11 @@ public class BlogController {
 	/*
 	 * =============================================================================
 	 * DELETE MAPPING FOR DELETING AN EXISTING BLOG
+	 * 
+	 * If blog id is valid and deletion is successful then Response code: 200 (OK)
+	 * If blog id is invalid then Response code: 404 (Not Found)
 	 */
+
 	@DeleteMapping("/blogs/{id}")
 	public ResponseEntity<BlogDto> deleteBlog() {
 		// have to write the code
@@ -58,7 +94,12 @@ public class BlogController {
 
 	/*
 	 * =============================================================================
-	 * GET MAPPING FOR FETCHING ALL THE COMMENT FROM A PARTICULAR BLOG
+	 * GET MAPPING FOR FETCHING ALL THE COMMENT FROM A PARTICULAR BLOG 
+	 * 
+	 * If blog id is valid and comments are present then Response code: 200 (OK) 
+	 * If blog id is valid but no comments are present then Response code: 204 (No Content)
+	 * If blog id is invalid then Response code: 404 (Not Found)
+	 * 
 	 */
 
 	@GetMapping("/blogs/{id}/comment")
@@ -70,6 +111,12 @@ public class BlogController {
 	/*
 	 * =============================================================================
 	 * POST MAPPING TO ADDING A NEW COMMENT ON THE EXISITING BLOG
+	 * 
+	 * 
+	 * If blog id is valid and comment is added successfully then Response code: 201 (Created) 
+	 * If blog id is invalid then Response code: 404 (Not Found) 
+	 * If request body is invalid then Response code: 400 (Bad Request)
+	 * 
 	 */
 
 	@PostMapping("/blogs/{id}/comment")
